2985ef8652e92c21ac4e483788a62f80bf8964f0
zenialexandre <zenialexandre123@gmail.com>
2024-03-20 15:27:20 -0300

    feat/chore: adding window setup and TODO for window_icon / cargo changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use bevy::{
+    prelude::*,
+    window::{EnabledButtons, WindowPosition},
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Asteroids".into(),
+                    resizable: false,
+                    enabled_buttons: EnabledButtons{ minimize: true, maximize: false, close: true },
+                    focused: true,
+                    position: WindowPosition::Centered(MonitorSelection::Primary),
+                    resolution: (800., 500.).into(),
+                    ..default()
+                }),
+                ..default()
+            })
+        )
+        .add_systems(Startup, setup)
+        .run();
 }
+
+fn setup(mut commands: Commands) {
+    let camera_2d_bundle: Camera2dBundle = Camera2dBundle {
+        camera: Camera { clear_color: ClearColorConfig::Custom(Color::BLACK), ..default() },
+        ..default()
+    };
+    commands.spawn(camera_2d_bundle);
+}
+
+/*
+TODO
+fn set_window_icon(
+    windows: NonSend<WinitWindows>,
+    primary_window_query: Query<Entity, With<PrimaryWindow>>
+) {
+    let primary_window_entity: Entity = primary_window_query.single();
+    let primary_window = windows.get_window(primary_window_entity).unwrap();
+
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open("assets/icons/asteroids.png")
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+
+    let icon: Icon = Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
+    primary_window.set_window_icon(Some(icon));
+}
+*/
