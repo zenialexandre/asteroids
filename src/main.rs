837e417a01f9a7738d397cfd35d4f62868d9b861
Alexandre <zenialexandre123@gmail.com>
2024-03-21 21:04:29 -0300

    feat: starting the collision development

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use bevy::{
-    prelude::*,
-    window::{EnabledButtons, PrimaryWindow, WindowPosition},
-    winit::WinitWindows
+    prelude::*, transform, window::{EnabledButtons, PrimaryWindow, WindowPosition}, winit::WinitWindows
 };
 use std::io::Cursor;
 use winit::window::Icon;
@@ -29,7 +27,7 @@ fn main() {
             })
         )
         .add_systems(Startup, (setup, set_game_window_icon))
-        .add_systems(FixedUpdate, set_hero_ship_movement)
+        .add_systems(FixedUpdate, (set_hero_ship_movement, set_new_hero_position_after_border_collision))
         .run();
 }
 
@@ -87,7 +85,7 @@ fn set_hero_ship_movement(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut query: Query<(&HeroShip, &mut Transform)>,
 ) {
-    let (hero_ship, mut transform) = query.single_mut();
+    let (hero_ship, mut hero_ship_transform) = query.single_mut();
     let mut rotation_factor: f32 = 0.0;
     let mut movement_factor: f32 = 0.0;
 
@@ -112,12 +110,35 @@ fn set_hero_ship_movement(
         movement_factor += 1.0;
     }
 
-    transform.rotate_z(
+    hero_ship_transform.rotate_z(
         rotation_factor * hero_ship.rotation_speed * time.delta_seconds()
     );
 
-    let movement_direction: Vec3 = transform.rotation * Vec3::Y;
+    let movement_direction: Vec3 = hero_ship_transform.rotation * Vec3::Y;
     let movement_distance: f32 = movement_factor * hero_ship.movement_speed * time.delta_seconds();
     let translation_delta: Vec3 = movement_direction * movement_distance;
-    transform.translation += translation_delta;
+    hero_ship_transform.translation += translation_delta;
 }
+
+/*
+TODO: Gonna need to set Sprite Bundles for the borders
+fn set_new_hero_position_after_border_collision(
+    time: Res<Time>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    hero_ship_query: Query<(&HeroShip, &mut Transform)>
+) {
+    let primary_window_entity: &Window = window_query.single();
+    let primary_window_width: u16 = primary_window_entity.width() as u16;
+    let primary_window_height: u16 = primary_window_entity.height() as u16;
+
+    println!("Window W: {}", primary_window_width);
+    println!("Window H: {}", primary_window_height);
+
+    let (_, hero_ship_transform) = hero_ship_query.single();
+    let hero_ship_x_position: u16 = hero_ship_transform.translation.x as u16;
+    let hero_ship_y_position: u16 = hero_ship_transform.translation.y as u16;
+
+    println!("Hero Ship X: {}", hero_ship_x_position);
+    println!("Hero Ship Y: {}", hero_ship_y_position);
+}
+*/
